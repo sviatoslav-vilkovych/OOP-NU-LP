5943c1533d8ad7108a27e5f2824cbf17be827f1b
Svyatoslav Vilkovych <VilSvat@gmail.com>
2016-10-17 10:52:10 +0300

    Add Lab3Version1 construction (2).

--- LabWorks/Lab3Version1/CashRegister.cpp
+++ LabWorks/Lab3Version1/CashRegister.cpp
@@ -27,16 +27,32 @@ CashRegister::CashRegister()
 	work_hours[0][6] = -1;
 	work_hours[1][6] = -1;
 }
-CashRegister::CashRegister(int, size_t, size_t **, map<char*, int>)
+CashRegister::CashRegister(int max_amount_of_moneyL, size_t left_paperL, size_t **work_hoursL, map<char*, int> avaliable_flightL)
 {
+	number = freeCashNum;
+	++freeCashNum;
 
+	num_sold_tickets = 0;
+	earned_money = 0;
+	max_amount_of_money = max_amount_of_moneyL;
+	left_paper = left_paperL;
+
+	passenger = new char[6];
+	passenger = "empty\0";
+
+	// from M to F = 8-18; Sat,Sun = holiday;
+	for (int j = 0; j < 7; ++j)
+	{
+		work_hours[0][j] = work_hoursL[0][j];
+		work_hours[1][j] = work_hoursL[1][j];
+	}
 }
 CashRegister::CashRegister(const CashRegister &obj)
 {
 }
 CashRegister::~CashRegister() 
 {
-	delete this
+	delete[] passenger;
 };
 // output
 void CashRegister::printClass()
